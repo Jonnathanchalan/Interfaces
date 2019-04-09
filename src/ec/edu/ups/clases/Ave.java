8627110f010132f572080e9cffb3224d35a98cb6
jonnathan <jonnathan@LAPTOP-U2SAQOEF>
2019-04-08 19:34:18 -0500

    inicializar proyecto

---
+++ src/ec/edu/ups/clases/Ave.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+import ec.edu.ups.clasesInterfaces.Acciones;
+
+/**
+ *
+ * @author jonnathan
+ */
+
+ public class Ave extends Animal implements Acciones{
+
+    public Ave(String nombre, int edad, double peso, String sexo) {
+        super(nombre, edad, peso, sexo);
+    }
+
+    @Override
+    public void comer() {
+      System.out.println("El ave "+this.getNombre()+" esta comiendo");  
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println("El ave "+this.getNombre()+" esta durmiendo");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println("El ave "+this.getNombre()+" esta moviendose");
+    }
+
+    @Override
+    public void jugar() {
+        System.out.println("El ave "+this.getNombre()+" esta jugando");
+    }
+
+    @Override
+    public void reproducirse() {
+        System.out.println("El ave "+this.getNombre()+" esta reproduciendose");
+    }
+    
+}   
+
